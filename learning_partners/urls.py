3561671a8747de62d873872939ee1cac574a97c3
Naman Kumar <namank_11@outlook.com>
2020-06-17 19:25:04 +0530

    four new templates added

---
+++ learning_partners/urls.py
@@ -0,0 +1,12 @@
+# Defines urlpatterns for learning_partners
+
+from django.urls import path
+from . import views
+
+app_name = 'learning_partners'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('/topics', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+]
--- learning_partners/views.py
+++ learning_partners/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Topic
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'learning_partners/index.html')
+
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_partners/topics.html', context)
+
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_partners/topic.html', context)
