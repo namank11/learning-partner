1b2fa6acc1f871364f1e0b0fb8ad8a100fb8d453
Naman Kumar <namank_11@outlook.com>
2020-06-21 08:00:34 +0530

    Ready For Deployment On Heroku

--- learning_partner/settings.py
+++ learning_partner/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -27,13 +26,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
     # My Apps
     'learning_partners',
     'users',
+    # Third Party Apps
+    'bootstrap4',
     # Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -73,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'learning_partner.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -83,7 +82,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -103,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -117,7 +114,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
@@ -125,3 +121,8 @@ STATIC_URL = '/static/'
 
 # MySettings
 LOGIN_URL = 'users:login'
+
+# HerokuSettings
+import django_heroku
+
+django_heroku.settings(locals())
